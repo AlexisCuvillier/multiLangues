f629de5db56504d84bd5bd62a145ad4cf31da937
AlexisCuvillier <alexis.cuvillier@protonmail.com>
2022-05-25 13:52:35 +0200

    multi

---
+++ src/Components/ToggleLangs/ToggleLangs.js
@@ -0,0 +1,19 @@
+import {useContext} from 'react'
+import FrenchFlag from '../../assets/france.svg'
+import EnglishFlag from '../../assets/united-kingdom.svg'
+import SpanishFlag from '../../assets/spain.svg'
+import './ToggleLangs.css'
+import { Context } from '../../context/langContext'
+
+export default function ToggleLangs() {
+
+  const {toggleLang} = useContext(Context)
+
+  return (
+    <div className='container-langs'>
+            <img onClick={() => toggleLang('FR')} src={FrenchFlag} alt = 'French Flag' />
+            <img onClick={() => toggleLang('EN')} src={EnglishFlag} alt= 'English Flag' />
+            <img onClick={() => toggleLang('ES')} src={SpanishFlag} alt= 'Spanish Flag' />
+    </div>
+  )
+}
